ee925e5e752ec8aa0eabca89a53fb82ca72d79e7
TRETYAKweb <tretyakbro@gmail.com>
2023-01-02 16:05:00 +0300

    fix(component): adding a component: Feed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 // Core
-
+import { Feed } from './components/Feed';
 // Components
 
 export const App = () => {
     return (
         <>
-            <section className = 'greeting'>
-                <h1>Ласкаво просимо на професійний курс з Rect, Redux, MobX, TypeScript.</h1>
-                <p>Репозиторий для виконання домашніх завдань</p>
-            </section>
+            <Feed />
         </>
     );
 };
