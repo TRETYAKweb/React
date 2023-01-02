1d1a6e028a1d41bfb076acf0c98f8529f12d3323
TRETYAKweb <tretyakbro@gmail.com>
2023-01-02 16:02:19 +0300

    chore(prettier): prettier settings

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    printWidth: 80,
+    singleQuote: true,
+    useTabs: false,
+    tabWidth: 4,
+    semi: true,
+    trailingComma: 'all',
+    bracketSpacing: true,
+    arrowParens: 'always',
+    importOrderSeparation: false,
+    endOfLine: 'auto',
+    jsxSingleQuote: true,
+    jsxBracketSameLine: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    arrowParens: 'avoid',
+};
