9c6898a3899fb18b38363c4771e3de20d25caa6e
TRETYAKweb <tretyakbro@gmail.com>
2023-01-08 06:29:14 +0300

    refactor(components): :recycle: recent-comments

--- src/components/RecentComments/index.js
+++ src/components/RecentComments/index.js
@@ -1,14 +1,18 @@
+import commentJson from '../../data/mock-data/comments.json';
+
 const Comment = () => {
     return (
         <>
-            <div className='comment'>
-                <p className='name'>Chuck Norris</p>
-                <time>1 day ago</time>
-                <p className='body'>788778</p>
-                <a href='/rtx-homeworks/feed/1d180bec-6e45-4e29-91f5-7339625fcb1c/comments'>
+            {commentJson.map((it) => <li className='comment' key={it.hash}>
+                <a href={`/users/${it.author.hash}`}>
+                    <p className='name'>{it.author.name}</p>
+                </a>
+                <time>{it.created}</time>
+                <p className='body'>{it.body}</p>
+                <a href={`/rtx-homeworks/feed/${it.post.hash}`}>
                     Больше комментариев к посту
                 </a>
-            </div>
+            </li>)}
         </>
     );
 };
@@ -17,11 +21,10 @@ export const RecentComments = () => {
     return (
         <div className='most-recent-comments'>
             <h1 className='title'>Популярные комментарии</h1>;
-            <section>
+            <ul>
                 <Comment />
-                <Comment />
-                <Comment />
-            </section>
+            </ul>
         </div>
     );
 };
+
