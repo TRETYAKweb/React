77590622e1b84a6a29c731ce6e50fc2cd3e8e7f6
TRETYAKweb <tretyakbro@gmail.com>
2023-01-08 06:41:07 +0300

    feat(components): :sparkles: add comment-form

---
+++ src/components/forms/CommentForm/index.js
@@ -0,0 +1,36 @@
+import propTypes from 'prop-types';
+
+export const CommentForm = ({ addComment, showComments }) => {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const formData = new FormData(form);
+        const comment = formData.get('body');
+        addComment(comment);
+        form.reset();
+    };
+
+    return <>
+        <form onSubmit={handleSubmit} className='commentForm'>
+            <img
+                src='https://placeimg.com/256/256/animals' alt='avatar'
+                className='comment-avatar' />
+
+            <label>
+                <div>
+                    <span className='error-message'></span>
+                </div>
+                <input
+                    placeholder='Комментарий...' type='text'
+                    name='body' />
+            </label>
+
+            <button type='submit'>Комментировать</button>
+        </form>
+    </>;
+};
+
+CommentForm.propTypes = {
+    addComment:   propTypes.func,
+    showComments: propTypes.func,
+};
