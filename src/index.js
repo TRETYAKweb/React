2914311b294ce76977d4b608e586a53d607c4f82
TRETYAKweb <tretyakbro@gmail.com>
2023-01-16 01:24:16 +0300

    fix(core): connect QueryClientProvider, ToastContainer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 // Core
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { ToastContainer, toast } from 'react-toastify';
 
 // Styles
 import 'react-toastify/dist/ReactToastify.css';
@@ -8,11 +11,16 @@ import './theme/init.scss';
 
 // Instruments
 
+// lib
+import { queryClient } from './lib';
+
 // App
 import { App } from './App';
 
 createRoot(document.getElementById('root')).render(
-    <>
+    <QueryClientProvider client={queryClient} >
         <App />
-    </>,
+        <ReactQueryDevtools initialIsOpen = { false } />
+        <ToastContainer />
+    </QueryClientProvider>,
 );
