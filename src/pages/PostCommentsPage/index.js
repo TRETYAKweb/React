672926a0f2732c6ca354622a2f69f50219d4269a
TRETYAKweb <tretyakbro@gmail.com>
2023-01-20 09:15:32 +0300

    feat(pages): add PostCommentsPage

---
+++ src/pages/PostCommentsPage/index.js
@@ -0,0 +1,8 @@
+import { Navigation } from '../../components/Navigation';
+import { Footer } from '../../components/footer';
+import { Profile } from '../../components/Profile';
+import { PostComments  } from '../../components/PostComments';
+
+export const PostCommentsPage = () => {
+    return <PostComments />;
+};
