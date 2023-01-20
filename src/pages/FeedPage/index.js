8397861feb1ccbad224bfb94a850350f4c3b113b
TRETYAKweb <tretyakbro@gmail.com>
2023-01-20 09:15:44 +0300

    feat(pages): add FeedPage

---
+++ src/pages/FeedPage/index.js
@@ -0,0 +1,11 @@
+import { Navigation } from '../../components/Navigation';
+import { Feed } from '../../components/Feed';
+import { Footer } from '../../components/footer';
+
+export const FeedPage = () => {
+    return (
+        <>
+            <Feed />
+        </>
+    );
+};
