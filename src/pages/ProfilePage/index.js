918f83654e5877082506c72fd8004c04c639a5f8
TRETYAKweb <tretyakbro@gmail.com>
2023-01-20 09:14:56 +0300

    feat(pages): add ProfilePage

---
+++ src/pages/ProfilePage/index.js
@@ -0,0 +1,7 @@
+import { Navigation } from '../../components/Navigation';
+import { Footer } from '../../components/footer';
+import { Profile } from '../../components/Profile';
+
+export const ProfilePage = () => {
+    return <Profile />;
+};
