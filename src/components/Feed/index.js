953db4e56420717d6cf9379a6962f459530077a0
TRETYAKweb <tretyakbro@gmail.com>
2023-01-20 09:18:42 +0300

    refactor(components): remove unnecessary elements

--- src/components/Feed/index.js
+++ src/components/Feed/index.js
@@ -1,26 +1,16 @@
-import { Navigation } from '../Navigation';
 import { Composer } from '../forms/Composer';
 import { PostsContainer } from '../Post';
 import { RecentComments } from '../RecentComments';
-import { Footer } from '../footer';
 
 export const Feed = () => {
     return (
         <>
-            <main>
-                <div className='feed-wrapper'>
-                    <div className='container'>
-                        <Navigation />
-                        <div className='posts'>
-                            <h1 className='title'>Стена</h1>
-                            <Composer />
-                            <PostsContainer />
-                        </div>
-                        <RecentComments />
-                    </div>
-                </div>
-            </main>
-            <Footer />
+            <div className='posts'>
+                <h1 className='title'>Стена</h1>
+                <Composer />
+                <PostsContainer />
+            </div>
+            <RecentComments />
         </>
     );
 };
