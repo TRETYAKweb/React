61a13d9f2c9f5b62b54914795133f38bbc92c0e0
TRETYAKweb <tretyakbro@gmail.com>
2023-01-22 09:26:38 +0300

    chore(hooks): re-export: useSignUp, useLogin, useUpdateProfile, useProfile

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,2 +1,6 @@
 export { usePosts } from './usePosts';
 export { useRecentComments } from './useRecentComments';
+export { useSignUp } from './useSignUp';
+export { useLogin } from './useLogin';
+export { useUpdateProfile  } from './useUpdateProfile';
+export { useProfile  } from './useProfile';
