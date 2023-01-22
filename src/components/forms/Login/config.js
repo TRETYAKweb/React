5b584c4288e61ab909f58eae65d3be8a5b552670
TRETYAKweb <tretyakbro@gmail.com>
2023-01-22 09:36:05 +0300

    feat(forms): config for LoginForm

---
+++ src/components/forms/Login/config.js
@@ -0,0 +1,11 @@
+import * as yup from 'yup';
+
+// eslint-disable-next-line no-template-curly-in-string
+const tooShortMessage = 'Минимальная длина - ${min} символов';
+// eslint-disable-next-line no-template-curly-in-string
+const tooLongMessage = 'максимальная длина - ${max} символов';
+
+export const schema  = yup.object().shape({
+    email:    yup.string().email().required('*Обязательное Поле'),
+    password: yup.string().min(8, tooShortMessage).max(16, tooLongMessage).required('*Обязательное поле'),
+});
