5e66d6eeead4ec15e7971e63ef12bc12966d0d2c
TRETYAKweb <tretyakbro@gmail.com>
2023-01-22 09:24:22 +0300

    chore(pages): re-export: SignUpPage, LoginPage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
 export { FeedPage } from './FeedPage';
 export { ProfilePage } from './ProfilePage';
 export { PostCommentsPage } from './PostCommentsPage';
+export { SignUpPage } from './SignUpPage';
+export { LoginPage } from './LoginPage';
