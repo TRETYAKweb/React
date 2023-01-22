836ea5df21a06a205b20ecc76e70c07a3f1351d8
TRETYAKweb <tretyakbro@gmail.com>
2023-01-22 09:23:36 +0300

    feat(pages): cleaned imports

--- src/pages/PostCommentsPage/index.js
+++ src/pages/PostCommentsPage/index.js
@@ -1,6 +1,3 @@
-import { Navigation } from '../../components/Navigation';
-import { Footer } from '../../components/footer';
-import { Profile } from '../../components/Profile';
 import { PostComments  } from '../../components/PostComments';
 
 export const PostCommentsPage = () => {
