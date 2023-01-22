5f84a94364b4e2a49c292939e18d81fb5ff84f40
TRETYAKweb <tretyakbro@gmail.com>
2023-01-22 09:38:42 +0300

    fix(core): connect routes for SignUpPage & LoginPage

--- src/App.js
+++ src/App.js
@@ -3,21 +3,29 @@ import {
     Routes, Route, Navigate,
 } from 'react-router-dom';
 
-import { LayoutWithNavigation } from './components/LayoutWithNavigation';
+import { MainLayout } from './components/MainLayout';
 
 // Pages
 
-import { FeedPage, ProfilePage, PostCommentsPage } from './pages';
+import {
+    FeedPage, ProfilePage, PostCommentsPage, SignUpPage, LoginPage,
+} from './pages';
 
 export const App = () => {
     return <>
 
         <Routes>
 
-            <Route element={<LayoutWithNavigation />}>
+            <Route element={<MainLayout hasNav />}>
                 <Route path='/feed' element={<FeedPage />} />
                 <Route path='/feed/:postId' element={<PostCommentsPage />} />
                 <Route path='/profile' element={<ProfilePage />} />
+            </Route>
+
+
+            <Route element={<MainLayout />}>
+                <Route path='/signup' element={<SignUpPage />}></Route>
+                <Route path='/login' element={<LoginPage />}></Route>
             </Route>
 
             <Route path='*' element={ <Navigate to ='/feed' replace />} />
