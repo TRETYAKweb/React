c0854703a5d0d1797f034dd85de202e369964061
TRETYAKweb <tretyakbro@gmail.com>
2023-01-27 06:32:17 +0300

    refactor(forms): schema for ProfileForm

--- src/components/forms/SignUp/config.js
+++ src/components/forms/SignUp/config.js
@@ -10,5 +10,5 @@ export const schema = yup.object().shape({
     name:            yup.string().min(5, tooShortMessage).max(40, tooLongMessage).required('*Обязательное поле'),
     email:           yup.string().email('Email must be a valid email').required('*Обязательное поле'),
     password:        yup.string().min(8, tooShortMessage).max(16, tooLongMessage).required('*Обязательное поле'),
-    confirmPassword: yup.string().oneOf([yup.ref('password')], 'Пароли должны совпадать').required('*'),
+    confirmPassword: yup.string().oneOf([yup.ref('password')], 'Пароли должны совпадать').required('*Обязательное поле'),
 });
