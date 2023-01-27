060847a8d5e0b92165609b1aa19ab24af97bcbd4
TRETYAKweb <tretyakbro@gmail.com>
2023-01-27 06:30:39 +0300

    refactor(hooks): useLogin

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,27 +1,16 @@
 import { useMutation } from 'react-query';
-import { useEffect, useContext } from 'react';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useProfile } from './useProfile';
 import { api } from '../api/api';
-
-import { authContext } from '../lib/authContext';
 
 
 export const useLogin = () => {
     const navigate = useNavigate();
-    const { setAuthData } = useContext(authContext);
+
 
     const mutation = useMutation((credential) => {
         return api.auth.login(credential);
     });
-
-    // const { data, isSuccess } =  useProfile();
-
-    // useEffect(() => {
-    //     if (data.data && isSuccess) {
-    //         setAuthData(data.data);
-    //     }
-    // }, [data, isSuccess]);
 
 
     useEffect(() => {
