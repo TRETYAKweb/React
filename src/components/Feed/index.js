b356c8555f4c29da119317cd5a369385563b7c18
TRETYAKweb <tretyakbro@gmail.com>
2023-01-27 06:34:48 +0300

    feat(components): import: useContext, useEffect, useProfile, authContext

--- src/components/Feed/index.js
+++ src/components/Feed/index.js
@@ -1,8 +1,20 @@
+import { useContext, useEffect } from 'react';
 import { Composer } from '../forms/Composer';
 import { PostsContainer } from '../Post';
 import { RecentComments } from '../RecentComments';
+import { useProfile } from '../../hooks';
+import { authContext } from '../../lib/authContext';
 
 export const Feed = () => {
+    const { data, isSuccess } = useProfile();
+    const { setAuthData } = useContext(authContext);
+
+    useEffect(() => {
+        if (data && isSuccess) {
+            setAuthData(data);
+        }
+    }, [data, isSuccess]);
+
     return (
         <>
             <div className='posts'>
