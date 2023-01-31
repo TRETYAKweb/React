5b48a5cc86edc7305141648a38c92f804091d8b8
TRETYAKweb <tretyakbro@gmail.com>
2023-01-31 03:45:42 +0300

    fix(hooks): useUpdateProfile: add invalidate profile

--- src/hooks/useUpdateProfile.js
+++ src/hooks/useUpdateProfile.js
@@ -1,10 +1,16 @@
-import { useMutation } from 'react-query';
+import { useMutation, useQueryClient } from 'react-query';
 
 import { api } from '../api/api';
 
 export const useUpdateProfile = () => {
+    const queryClient = useQueryClient();
+
     const mutation = useMutation((data) => {
         return api.profile.updateProfile(data);
+    }, {
+        onSuccess: () => {
+            queryClient.invalidateQueries('profile');
+        },
     });
 
     return mutation;
