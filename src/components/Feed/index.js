c9b3d3441f10bccb5dbc780bd153ea15015a070c
TRETYAKweb <tretyakbro@gmail.com>
2023-01-31 03:48:52 +0300

    fix(components): Feed: remove unnecessary logic

--- src/components/Feed/index.js
+++ src/components/Feed/index.js
@@ -3,18 +3,8 @@ import { Composer } from '../forms/Composer';
 import { PostsContainer } from '../Post';
 import { RecentComments } from '../RecentComments';
 import { useProfile } from '../../hooks';
-import { AppContext } from '../../lib';
 
 export const Feed = () => {
-    const { data, isSuccess } = useProfile();
-    const { setAuthData } = useContext(AppContext);
-
-    useEffect(() => {
-        if (data && isSuccess) {
-            setAuthData(data);
-        }
-    }, [data, isSuccess]);
-
     return (
         <>
             <div className='posts'>
