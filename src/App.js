aab20190cdf06a3bb1053223ee0df2c5e1376d19
TRETYAKweb <tretyakbro@gmail.com>
2023-01-31 03:49:11 +0300

    feat(core): add error handling and get auth data

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@
 import {
     Routes, Route, Navigate,
 } from 'react-router-dom';
-
+import { observer } from 'mobx-react-lite';
+import { useEffect } from 'react';
+import { toast } from 'react-toastify';
 import { MainLayout } from './components/MainLayout';
+import { useStore, useAuth } from './hooks';
 
 // Pages
 
@@ -11,7 +14,31 @@ import {
     FeedPage, ProfilePage, PostCommentsPage, SignUpPage, LoginPage,
 } from './pages';
 
-export const App = () => {
+export const App = observer(() => {
+    const { uiStore } = useStore();
+
+    useAuth();
+
+    // console.log(error, 'error');
+
+    useEffect(() => {
+        if (uiStore.errorMessage) {
+            const notify = () => toast.error(uiStore.errorMessage, {
+                position:        'top-right',
+                autoClose:       5000,
+                hideProgressBar: false,
+                closeOnClick:    true,
+                pauseOnHover:    true,
+                draggable:       true,
+                progress:        undefined,
+            });
+
+            notify();
+
+            uiStore.resetError();
+        }
+    }, [uiStore.errorMessage]);
+
     return <Routes>
 
         <Route element={<MainLayout hasNav />}>
@@ -29,4 +56,4 @@ export const App = () => {
         <Route path='*' element={ <Navigate to ='/feed' replace />} />
 
     </Routes>;
-};
+});
