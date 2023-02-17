7e3d4b8c0d11a21c4879489b029772b3e0896982
TRETYAKweb <tretyakbro@gmail.com>
2023-02-17 12:02:48 +0300

    fix(hooks): remove useStore && add useDispatch, authActions

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,22 +1,22 @@
 import { useMutation } from 'react-query';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import { api } from '../api/api';
-import { useStore } from './useStore';
+import { authActions } from '../lib/redux/actions';
 
 
 export const useLogin = () => {
-    const { authStore } = useStore();
     const navigate = useNavigate();
+    const dispatch = useDispatch();
 
     const mutation = useMutation((credentials) => api.auth.login(credentials));
 
     useEffect(() => {
-        const token = mutation.data?.data;
-
-        if (mutation.isSuccess && token) {
-            localStorage.setItem('token', token);
-            authStore.setToken(token);
+        if (mutation.isSuccess) {
+            const data = mutation.data?.data;
+            localStorage.setItem('token', data);
+            dispatch(authActions.setToken(data));
             navigate('/feed');
         }
     }, [mutation.isSuccess]);
