0964a067c14b06f6993f877e7942f7bdb8c73a01
TRETYAKweb <tretyakbro@gmail.com>
2023-02-17 12:04:15 +0300

    fix(core): cleaning mobx && connect redux provider

--- src/index.js
+++ src/index.js
@@ -5,38 +5,28 @@ import { createRoot } from 'react-dom/client';
 import { QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { ToastContainer, toast } from 'react-toastify';
-import { configure } from 'mobx';
+
+// Context Provider
+import { Provider } from 'react-redux';
 
 // Styles
 import 'react-toastify/dist/ReactToastify.css';
 import './theme/init.scss';
 
-// Instruments
-
-// Context Provider
-import { StoreProvider  } from './lib/storeContext';
-
-
 // lib
 import { queryClient } from './lib';
+import { store } from './lib/redux/init/store';
 
 // App
 import { App } from './App';
 
-configure({
-    enforceActions:             'always',
-    computedRequiresReaction:   true,
-    observableRequiresReaction: true,
-    reactionRequiresObservable: true,
-});
-
 createRoot(document.getElementById('root')).render(
     <QueryClientProvider client={queryClient} >
-        <StoreProvider>
+        <Provider store={store}>
             <Router>
                 <App />
             </Router>
-        </StoreProvider>
+        </Provider>
         <ReactQueryDevtools initialIsOpen = { false } />
         <ToastContainer />
     </QueryClientProvider>,
