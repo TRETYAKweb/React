632bd652a4c096a333a11ad27e58cf0d22805e6f
TRETYAKweb <tretyakbro@gmail.com>
2023-04-09 23:56:48 +0300

    refactor: remove unused components from ProfilePage

--- src/pages/ProfilePage/index.js
+++ src/pages/ProfilePage/index.js
@@ -1,5 +1,3 @@
-import { Navigation } from '../../components/Navigation';
-import { Footer } from '../../components/footer';
 import { ProfileForm } from '../../components/forms/Profile';
 
 export const ProfilePage = () => {
