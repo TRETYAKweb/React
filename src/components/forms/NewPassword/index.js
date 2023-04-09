2f70425759405b187e95fc32cfe585be12b978e5
TRETYAKweb <tretyakbro@gmail.com>
2023-04-09 23:43:44 +0300

    feat: add NewPasswordForm component
    Added a new component called NewPasswordForm, which uses the react-hook-form library and yup validation schema to render a form for changing the user's password. The component also dispatches an async action to reset the user's password when the form is submitted.

---
+++ src/components/forms/NewPassword/index.js
@@ -0,0 +1,56 @@
+// Core
+import { useForm } from 'react-hook-form';
+import { NavLink } from 'react-router-dom';
+import { useEffect } from 'react';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+
+// Elements
+import { useDispatch } from 'react-redux';
+import { Input } from '../elements';
+
+// Other
+import { schema } from './config';
+import { authActions } from '../../../lib/redux/actions';
+
+export const NewPasswordForm = () => {
+    const dispatch = useDispatch();
+
+    const form = useForm({
+        mode:     'onTouched',
+        resolver: yupResolver(schema),
+    });
+
+    const handleSubmit = form.handleSubmit((user) => {
+        const { newPassword, oldPassword } = user;
+        dispatch(authActions.asyncResetPassword(newPassword, oldPassword));
+    });
+
+    useEffect(() => {
+        form.setFocus('oldPassword');
+    }, []);
+
+
+    return (
+        <form className='form' onSubmit={handleSubmit}>
+            <div className='wrapper'>
+                <div>
+                    <h1>Смена пароля</h1>
+
+                    <Input
+                        placeholder='Старый пароль'
+                        error = {form.formState.errors.oldPassword}
+                        register = {form.register('oldPassword')} />
+
+                    <Input
+                        placeholder='Новый пароль'
+                        error = {form.formState.errors.newPassword}
+                        register = {form.register('newPassword')} />
+
+                    <button className='loginSubmit' type='submit'>Сменить пароль</button>
+                </div>
+                <NavLink to={'/profile'}>← Назад</NavLink>
+            </div>
+        </form>
+    );
+};
