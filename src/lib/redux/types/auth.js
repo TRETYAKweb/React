b34780d6ae8078ea87678dfea3b80cb5b8ec107d
TRETYAKweb <tretyakbro@gmail.com>
2023-04-11 03:39:26 +0300

    refactor(auth): rename ASYNC_RESET_PASSWORD to RESET_PASSWORD_REQUEST

--- src/lib/redux/types/auth.js
+++ src/lib/redux/types/auth.js
@@ -1,4 +1,4 @@
 export const authTypes = Object.freeze({
-    SET_TOKEN:            'SET_TOKEN',
-    ASYNC_RESET_PASSWORD: 'ASYNC_RESET_PASSWORD',
+    SET_TOKEN:              'SET_TOKEN',
+    RESET_PASSWORD_REQUEST: 'RESET_PASSWORD_REQUEST',
 });
