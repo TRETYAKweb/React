54643a926f9063d4f0d34c7972518563ee461fbe
TRETYAKweb <tretyakbro@gmail.com>
2023-05-14 21:38:39 +0300

    refactor: Rename SignUp/config.js to SignUp/config.ts
    This commit renames the SignUp/config.js file to SignUp/config.ts to convert it to a TypeScript file. The schema object is updated to use the IAuthRequest type with an additional confirmPassword field.

--- src/components/forms/SignUp/config.ts
+++ src/components/forms/SignUp/config.ts
@@ -1,12 +1,13 @@
 // Core
 import * as yup from 'yup';
+import { IAuthRequest } from '../../../types';
 
 // eslint-disable-next-line no-template-curly-in-string
 const tooShortMessage = 'Минимальная длина - ${min} символов';
 // eslint-disable-next-line no-template-curly-in-string
 const tooLongMessage = 'максимальная длина - ${max} символов';
 
-export const schema = yup.object().shape({
+export const schema: yup.SchemaOf<IAuthRequest & { confirmPassword: string }> = yup.object().shape({
     name:            yup.string().min(5, tooShortMessage).max(40, tooLongMessage).required('*Обязательное поле'),
     email:           yup.string().email('Email must be a valid email').required('*Обязательное поле'),
     password:        yup.string().min(8, tooShortMessage).max(16, tooLongMessage).required('*Обязательное поле'),
