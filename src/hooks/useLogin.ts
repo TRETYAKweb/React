423ea682e202bbc2f2cce945190dcc4e82da0771
TRETYAKweb <tretyakbro@gmail.com>
2023-05-14 21:42:51 +0300

    refactor: Rename useLogin.js to useLogin.ts
    This commit renames the useLogin.js file to useLogin.ts to align with TypeScript conventions. The file extension has been changed to .ts to indicate that it contains TypeScript code instead of JavaScript.
    
    No functional changes have been made to the code itself.

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,16 +1,17 @@
 import { useMutation } from 'react-query';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
 import { api } from '../api/api';
 import { authActions } from '../lib/redux/actions';
+import { useAppDispatch } from '../lib/redux/init/store';
+import { IAuthRequest } from '../types';
 
 
 export const useLogin = () => {
     const navigate = useNavigate();
-    const dispatch = useDispatch();
+    const dispatch = useAppDispatch();
 
-    const mutation = useMutation((credentials) => api.auth.login(credentials));
+    const mutation = useMutation((credentials:IAuthRequest) => api.auth.login(credentials));
 
     useEffect(() => {
         if (mutation.isSuccess) {
