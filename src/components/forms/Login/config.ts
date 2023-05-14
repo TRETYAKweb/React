6582525b5eb21c6dbfe52cc53d76ef46de70d70f
TRETYAKweb <tretyakbro@gmail.com>
2023-05-14 21:35:32 +0300

    refactor: Rename Login/config.js to Login/config.ts
    This commit renames the Login/config.js file to Login/config.ts to convert it to a TypeScript file. The schema object is updated to use the IAuthRequest type.

--- src/components/forms/Login/config.ts
+++ src/components/forms/Login/config.ts
@@ -1,11 +1,12 @@
 import * as yup from 'yup';
+import { IAuthRequest } from '../../../types';
 
 // eslint-disable-next-line no-template-curly-in-string
 const tooShortMessage = 'Минимальная длина - ${min} символов';
 // eslint-disable-next-line no-template-curly-in-string
 const tooLongMessage = 'максимальная длина - ${max} символов';
 
-export const schema  = yup.object().shape({
+export const schema: yup.SchemaOf<Pick<IAuthRequest, 'email' | 'password'>>  = yup.object().shape({
     email:    yup.string().email().required('*Обязательное Поле'),
     password: yup.string().min(8, tooShortMessage).max(16, tooLongMessage).required('*Обязательное поле'),
 });
