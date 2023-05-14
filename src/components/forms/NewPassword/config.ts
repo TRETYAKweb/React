7c7bba63a4e24cd186b4343fc24bb97aa2a3deb9
TRETYAKweb <tretyakbro@gmail.com>
2023-05-14 21:36:56 +0300

    refactor: Rename NewPassword/config.js to NewPassword/config.ts
    This commit renames the NewPassword/config.js file to NewPassword/config.ts to convert it to a TypeScript file. The schema object is updated to use the IResetPassword type.

--- src/components/forms/NewPassword/config.ts
+++ src/components/forms/NewPassword/config.ts
@@ -1,12 +1,13 @@
 // Core
 import * as yup from 'yup';
+import { IResetPassword } from '../../../types';
 
 // eslint-disable-next-line no-template-curly-in-string
 const tooShortMessage = 'Минимальная длина - ${min} символов';
 // eslint-disable-next-line no-template-curly-in-string
 const tooLongMessage = 'максимальная длина - ${max} символов';
 
-export const schema = yup.object().shape({
+export const schema: yup.SchemaOf<IResetPassword> = yup.object().shape({
     oldPassword: yup.string().min(2, tooShortMessage).max(40, tooLongMessage).required('*Обязательное поле'),
     newPassword: yup.string().min(2, tooShortMessage).max(40, tooLongMessage).required('*Обязательное поле'),
 });
