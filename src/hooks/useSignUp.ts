1e376648f42b6379df84af50afe069e251e85118
TRETYAKweb <tretyakbro@gmail.com>
2023-05-14 21:44:17 +0300

    refactor: Rename useSignUp.js to useSignUp.ts
    This commit renames the useSignUp.js file to useSignUp.ts to align with TypeScript conventions. The file extension has been changed to .ts to indicate that it contains TypeScript code instead of JavaScript.
    
    In addition, the type annotations have been added to the relevant variables and function parameters to ensure type safety and improved code readability.
    
    No functional changes have been made to the code itself.

--- src/hooks/useSignUp.ts
+++ src/hooks/useSignUp.ts
@@ -1,18 +1,22 @@
 import { useEffect } from 'react';
 import { useMutation } from 'react-query';
 import { useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { AxiosError } from 'axios';
 import { errorAction } from '../lib/redux/actions';
 import { api } from '../api/api';
+import { IApiErrorResponse, IAuthRequest } from '../types';
+import { useAppDispatch } from '../lib/redux/init/store';
 
 export const useSignUp = () => {
     const navigation = useNavigate();
 
-    const dispatch = useDispatch();
+    const dispatch = useAppDispatch();
 
-    const mutation = useMutation((user) => api.auth.signup(user), {
-        onError: (error) => {
-            dispatch(errorAction.setError(error.response.data.message));
+    const mutation = useMutation((user: IAuthRequest) => api.auth.signup(user), {
+        onError: (error: AxiosError<IApiErrorResponse>) => {
+            if (error.response) {
+                dispatch(errorAction.setError(error.response.data.message));
+            }
         },
     });
 
