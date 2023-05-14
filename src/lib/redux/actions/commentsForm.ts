f24ca351c6c00b41bd7616d4186fc492cb57bb0b
TRETYAKweb <tretyakbro@gmail.com>
2023-05-14 21:46:11 +0300

    refactor: Rename src/lib/redux/actions/commentsForm.js to src/lib/redux/actions/commentsForm.ts
    This commit renames the src/lib/redux/actions/commentsForm.js file to src/lib/redux/actions/commentsForm.ts to indicate that it now contains TypeScript code instead of JavaScript. The file extension has been changed to .ts to align with TypeScript conventions.
    
    Additionally, the action creators in the file have been updated to include TypeScript type annotations for the payload and action types.
    
    No functional changes have been made to the code itself.

--- src/lib/redux/actions/commentsForm.ts
+++ src/lib/redux/actions/commentsForm.ts
@@ -1,7 +1,8 @@
+import { IPayloadAction } from '../../../types/redux';
 import { commentsFormType } from '../types';
 
 export const commentsFormActions = Object.freeze({
-    setCurrentPostHash: (postHash) => {
+    setCurrentPostHash: (postHash: string): IPayloadAction<string> => {
         return {
             type:    commentsFormType.SET_CURRENT_POST_HASH,
             payload: postHash,
