ef4cc67be63460e61b9c6866581b9a08abae2da4
TRETYAKweb <tretyakbro@gmail.com>
2023-05-14 21:45:41 +0300

    refactor: Rename src/lib/redux/actions/auth.js to src/lib/redux/actions/auth.ts
    This commit renames the src/lib/redux/actions/auth.js file to src/lib/redux/actions/auth.ts to indicate that it now contains TypeScript code instead of JavaScript. The file extension has been changed to .ts to align with TypeScript conventions.
    
    Additionally, the action creators in the file have been updated to include TypeScript type annotations for the payload and action types.
    
    No functional changes have been made to the code itself.

--- src/lib/redux/actions/auth.ts
+++ src/lib/redux/actions/auth.ts
@@ -1,13 +1,17 @@
+import { IResetPassword } from '../../../types';
+import { IPayloadAction } from '../../../types/redux';
 import { authTypes } from '../types';
 
 export const authActions = Object.freeze({
-    setToken: (token) => {
+    setToken: (token: string): IPayloadAction<string> => {
         return {
             type:    authTypes.SET_TOKEN,
             payload: token,
         };
     },
-    resetPasswordRequest: (oldPassword, newPassword) => {
+    resetPasswordRequest: (data: IResetPassword): IPayloadAction<IResetPassword> => {
+        const { oldPassword, newPassword } = data;
+
         return {
             type:    authTypes.RESET_PASSWORD_REQUEST,
             payload: { oldPassword, newPassword },
