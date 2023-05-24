fbd5c3d050f0b2ceb808a89d5b36c9deaf134a55
TretyakWeb <tretyakbro@gmail.com>
2023-05-24 03:24:20 +0300

    feat(hooks): rename useAuth.js to useAuth.ts
    Rename the useAuth.js file in the hooks folder to useAuth.ts. This change reflects the updated file extension to TypeScript and adds type annotations to the useAuth hook.
    
    Diff:
    - Renamed useAuth.js file to useAuth.ts
    - Added type annotations to the onError callback function

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,17 +1,21 @@
 import { useQuery } from 'react-query';
 import { useDispatch } from 'react-redux';
+import { AxiosError } from 'axios';
 import { errorAction } from '../lib/redux/actions';
 
 // Api
 import { api } from '../api/api';
+import { IApiErrorResponse } from '../types';
 
 
 export const useAuth = () => {
     const dispatch = useDispatch();
 
     const query = useQuery('auth', api.auth.auth, {
-        onError: (error) => {
-            dispatch(errorAction.setError(error.response.data.message));
+        onError: (error:AxiosError<IApiErrorResponse>) => {
+            if (error.response) {
+                dispatch(errorAction.setError(error.response.data.message));
+            }
         },
     });
 
