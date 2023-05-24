96f2873d8d1338aa2eb460fe74b5bf885f419455
TretyakWeb <tretyakbro@gmail.com>
2023-05-24 03:40:43 +0300

    feat(hooks): convert useUpdateProfile.js to TypeScript
    Convert the useUpdateProfile.js file to TypeScript by renaming it to useUpdateProfile.ts and adding type annotations. The mutation function now expects an argument of type IProfileRequest. This update improves the codebase by leveraging TypeScript's static type checking.
    
    Diff:
    - Renamed useUpdateProfile.js to useUpdateProfile.ts
    - Added type annotations to the useUpdateProfile function
    
    Files:
    - src/hooks/useUpdateProfile.js (deleted)
    - src/hooks/useUpdateProfile.ts (added)

--- src/hooks/useUpdateProfile.ts
+++ src/hooks/useUpdateProfile.ts
@@ -1,11 +1,12 @@
 import { useMutation, useQueryClient } from 'react-query';
 
 import { api } from '../api/api';
+import { IProfileRequest } from '../types';
 
 export const useUpdateProfile = () => {
     const queryClient = useQueryClient();
 
-    const mutation = useMutation((data) => {
+    const mutation = useMutation((data: IProfileRequest) => {
         return api.profile.updateProfile(data);
     }, {
         onSuccess: () => {
